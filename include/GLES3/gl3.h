60385174addecb3addf8fbb4e85b6499de891191
xiaozhuai <798047000@qq.com>
2024-05-27 14:03:11 +0800

    Support iOS

---
+++ include/GLES3/gl3.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __glesx_gl3_h_
+#define __glesx_gl3_h_
+
+#if !defined(GLES_SILENCE_DEPRECATION)
+#define GLES_SILENCE_DEPRECATION
+#endif
+
+#include <OpenGLES/ES3/gl.h>
+
+#endif  // __glesx_gl3_h_
---
+++ include/GLES3/gl3ext.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __glesx_gl3ext_h_
+#define __glesx_gl3ext_h_
+
+#if !defined(GLES_SILENCE_DEPRECATION)
+#define GLES_SILENCE_DEPRECATION
+#endif
+
+#include <OpenGLES/ES3/glext.h>
+
+#endif  // __glesx_gl3ext_h_
