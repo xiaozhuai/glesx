4d80b76e8768ab8cdd095671a2aa839304614a62
xiaozhuai <798047000@qq.com>
2024-10-16 14:09:03 +0800

    Avoid absolute paths

---
+++ ios/include/GLES2/gl2.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __glesx_gl2_h_
+#define __glesx_gl2_h_
+
+#if !defined(GLES_SILENCE_DEPRECATION)
+#define GLES_SILENCE_DEPRECATION
+#endif
+
+#include <OpenGLES/ES2/gl.h>
+
+#endif  // __glesx_gl2_h_
